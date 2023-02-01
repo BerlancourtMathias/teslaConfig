6ab230916f2234ca3f158ecce9f5881036d4be43
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-01 21:17:12 +0100

    first commit without the full css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [performance, setPerformance] = useState(0);
+  const [colorBlack, setColorBlack] = useState(0);
+  let result = 90700;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <h1>Tesla Config</h1>
+      <div className="container">
+        <div className="column1">
+          <h2>Sélectionnez votre Véhicule</h2>
+          <button
+            onClick={() => {
+              setPerformance(0);
+            }}
+          >
+            Grande autonomie - 90 700 €
+          </button>
+          <button
+            onClick={() => {
+              setPerformance(1);
+            }}
+          >
+            Performance - 106 700 €
+          </button>
+        </div>
+        <div className="column2">
+          <h2>Sélectionnez la couleur</h2>
+          <button
+            onClick={() => {
+              setColorBlack(0);
+            }}
+          >
+            Blanc nacré multicouhces - 0 €
+          </button>
+          <button
+            onClick={() => {
+              setColorBlack(1);
+            }}
+          >
+            Noir uni - 1000 €
+          </button>
+        </div>
+        <div className="calc">
+          {performance ? (
+            <div>{(result += 16000)} </div>
+          ) : (
+            <div> {(result = 90700)}</div>
+          )}
+          {colorBlack ? (
+            <div>{(result += 1000)}</div>
+          ) : (
+            <div>{(result += 0)}</div>
+          )}
+        </div>
+        <div className="result">{result}</div>
+      </div>
+      <footer></footer>
     </div>
   );
-}
+};
 
 export default App;
